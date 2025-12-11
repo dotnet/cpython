292fc20497d5f9478dbfbf6ad64051e30dc4d3bd
Alexander Köplinger <alex.koeplinger@outlook.com>
2025-12-11 13:48:35 +0100

    Set -UseBasicParsing option for Invoke-WebRequest

--- PCbuild/find_python.bat
+++ PCbuild/find_python.bat
@@ -51,7 +51,7 @@
     @rem NB: Must use single quotes around NUGET here, NOT double!
     @rem Otherwise, a space in the path would break things
     @rem If it fails, retry with any available copy of Python
-    @powershell.exe -Command Invoke-WebRequest %_Py_NUGET_URL% -OutFile '%_Py_NUGET%'
+    @powershell.exe -Command Invoke-WebRequest %_Py_NUGET_URL% -UseBasicParsing -OutFile '%_Py_NUGET%'
     @if errorlevel 1 (
         @%_Py_HOST_PYTHON% -E "%~dp0\urlretrieve.py" "%_Py_NUGET_URL%" "%_Py_NUGET%"
     )
